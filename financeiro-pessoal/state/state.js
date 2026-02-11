db602b12eaa65d3841051f52bf9efb6b29395505
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-02-11 14:19:50 +0000

    funções para calculo de saldo total, total de receitas e total de despesas

--- financeiro-pessoal/state/state.js
+++ financeiro-pessoal/state/state.js
@@ -1,21 +1,57 @@
+export function calcularSaldo(lista) {
+   return lista.reduce((total, transacao) => {
+      if(transacao.tipo === "receita") {
+         total + transacao.valor;
+      } else {
+         total - transacao.valor;
+      }
+   },0);
+}
+
+
+export function calcularReceita(lista) {
+   return lista.reduce((total, transacao) => {
+      if(transacao.tipo === "receita") {
+         return total + transacao.valor;
+      }
+      return total;
+   }, 0);
+}
+
+
+export function calcularDespesa(lista) {
+   return lista.reduce((total, transacao) => {
+      if(transacao.tipo === "despesa") {
+         return total + transacao.valor;
+      }
+      return total;
+   }, 0);
+}
+
 /*
 OBJETIVO:
-Centralizar o controle das transações em memória.
+Calcular saldo total, total de receitas e total de despesas.
 
 PENSAMENTO:
 
-1) Carregar as transações salvas quando o sistema iniciar.
-2) Criar função para:
-   - Retornar lista atual.
-   - Adicionar nova transação.
-   - (Opcional) remover transação.
-3) Sempre que alterar o estado:
-   - Atualizar o localStorage.
+1) O saldo começa em 0.
+2) Para cada transação:
+   - Se for receita, soma.
+   - Se for despesa, subtrai.
+3) Para calcular totais separados:
+   - Filtrar por tipo.
+   - Somar valores.
 
-REFLEXÃO:
-- Por que não manipular o localStorage diretamente no UI?
-- O que significa separar responsabilidade?
+DICA IMPORTANTE:
+Use reduce().
 
-DESAFIO:
-Como garantir que o array nunca fique fora de sincronia?
+Pergunta:
+- O que é o acumulador?
+- Qual deve ser o valor inicial?
+
+Exemplo mental:
+[100, -50, 200]
+Resultado esperado: 250
+
+Não escreva loops tradicionais.
 */
